7af596884c44d673cc8c37b792253118258a6980
bahodirwebase <bahodir.hojiyev@webase.uz>
2026-01-22 17:04:20 +0500

    fix

--- src/views/dashboard/analytical/index.vue
+++ src/views/dashboard/analytical/index.vue
@@ -15,68 +15,20 @@
         <n-gi>
           <n-grid y-gap="18" x-gap="18" :cols="2">
             <n-gi>
-              <n-card :key="isDark">
-                <div class="card-header">
-                  <h3>Users</h3>
-                  <div>
-                    <n-icon :component="TrendingUpOutline" size="18" color="var(--color-success)"></n-icon>
-                    <span class="custom-badge bg-success">+12.5%</span>
-                  </div>
-                </div>
-                <h1>28.05k</h1>
-                <div class="chart-wrapper">
-                  <apexchart type="area" height="150" :options="(chartOptions as ApexOptions)" :series="series">
-                  </apexchart>
-                </div>
-              </n-card>
+              <statisticsByChart :key="(isDark as any)" :series="series" :chartOptions="chartOptionsSuccess"
+                :type="'success'" :label="'Users'" :value="'28.05k'" />
             </n-gi>
             <n-gi>
-              <n-card :key="isDark">
-                <div class="card-header">
-                  <h3>Sessions</h3>
-                  <div>
-                    <n-icon :component="TrendingUpOutline" size="18" color="var(--color-success)"></n-icon>
-                    <span class="custom-badge bg-success">+12.5%</span>
-                  </div>
-                </div>
-                <h1>97.66k</h1>
-                <div class="chart-wrapper">
-                  <apexchart type="area" height="150" :options="(chartOptions as ApexOptions)" :series="series">
-                  </apexchart>
-                </div>
-              </n-card>
+              <statisticsByChart :key="(isDark as any)" :series="series" :chartOptions="chartOptionsSuccess"
+                :type="'success'" :label="'Sessions'" :value="'97.66k'" />
             </n-gi>
             <n-gi>
-              <n-card :key="isDark">
-                <div class="card-header">
-                  <h3>Av. Visit Duration</h3>
-                  <div>
-                    <n-icon :component="TrendingDownOutline" size="18" color="var(--color-error)"></n-icon>
-                    <span class="custom-badge bg-error">-5.2%</span>
-                  </div>
-                </div>
-                <h1>3m 40ses</h1>
-                <div class="chart-wrapper">
-                  <apexchart type="area" height="150" :options="(chartOptions3 as ApexOptions)" :series="series3">
-                  </apexchart>
-                </div>
-              </n-card>
+              <statisticsByChart :key="(isDark as any)" :series="series" :chartOptions="chartOptionsError"
+                :type="'error'" :label="'Avg. Visit Duration'" :value="'3m 40ses'" />
             </n-gi>
             <n-gi>
-              <n-card >
-                <div class="card-header">
-                  <h3>Bounce rate</h3>
-                  <div>
-                    <n-icon :component="TrendingUpOutline" size="18" color="var(--color-success)"></n-icon>
-                    <span class="custom-badge bg-success">+12.5%</span>
-                  </div>
-                </div>
-                <h1>33.48%</h1>
-                <div class="chart-wrapper">
-                  <apexchart :key="(themeStore.isDark as any)" type="area" height="150" :options="(chartOptions as ApexOptions)" :series="series">
-                  </apexchart>
-                </div>
-              </n-card>
+              <statisticsByChart :key="(isDark as any)" :series="series" :chartOptions="chartOptionsSuccess"
+                :type="'success'" :label="'Bounce rate'" :value="'33.48%'" />
             </n-gi>
           </n-grid>
         </n-gi>
@@ -95,25 +47,27 @@
           <customerTransaction />
         </n-gi>
       </n-grid>
-      {{ themeStore.isDark }}
     </n-space>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watch, toRef } from "vue";
-import VueApexCharts from "vue3-apexcharts";
 import type { ApexOptions } from "apexcharts";
-import statistics from "./components/statistics.vue";
-import latestLeads from "./components/latestLeads.vue";
-import lineChart from "./components/lineChart.vue";
-import { ChatbubbleOutline, EyeOutline, HeartOutline, ShareSocialOutline, TrendingDownOutline, TrendingUpOutline, VideocamOutline } from "@vicons/ionicons5";
-import customerTransaction from "./components/customerTransaction.vue";
-import browserUsage from "./components/browserUsage.vue";
+
+import statistics from "./widgets/statistics.vue";
+import latestLeads from "./widgets/latestLeads.vue";
+import lineChart from "./widgets/lineChart.vue";
+import customerTransaction from "./widgets/customerTransaction.vue";
+import browserUsage from "./widgets/browserUsage.vue";
+import statisticsByChart from "./widgets/statisticsByChart.vue";
+
+import { ChatbubbleOutline, EyeOutline, HeartOutline, ShareSocialOutline, VideocamOutline } from "@vicons/ionicons5";
+
 import { useThemeStore } from "@/stores/theme";
 
 
-const apexchart = VueApexCharts;
+
 const themeStore = useThemeStore();
 
 // Create a proper ref for isDark
@@ -126,14 +80,7 @@ const series = ref([
   },
 ]);
 
-const series3 = ref([
-  {
-    name: "Revenue",
-    data: [31, 0, 32, 90, 120, 51, 42, 109, 100, 40, 35],
-  },
-]);
-
-const chartOptions = ref<ApexOptions>({
+const chartOptionsSuccess = ref<ApexOptions>({
   chart: {
     toolbar: { show: false }, // Ortiqcha tugmalarni o'chirish
     sparkline: { enabled: false }, // Card ichiga to'liq sig'ishi uchun
@@ -163,11 +110,11 @@ const chartOptions = ref<ApexOptions>({
   grid: { show: false }, // Setka chiziqlarini o'chirish (toza ko'rinish uchun)
   xaxis: { labels: { show: false }, axisBorder: { show: false }, axisTicks: { show: false } },
   yaxis: { show: false },
-  tooltip : {
+  tooltip: {
     theme: isDark.value ? 'dark' : 'light',
   }
 });
-const chartOptions3 = ref<ApexOptions>({
+const chartOptionsError = ref<ApexOptions>({
   chart: {
     toolbar: { show: false }, // Ortiqcha tugmalarni o'chirish
     sparkline: { enabled: false }, // Card ichiga to'liq sig'ishi uchun
@@ -197,7 +144,7 @@ const chartOptions3 = ref<ApexOptions>({
   grid: { show: false }, // Setka chiziqlarini o'chirish (toza ko'rinish uchun)
   xaxis: { labels: { show: false }, axisBorder: { show: false }, axisTicks: { show: false } },
   yaxis: { show: false },
-  tooltip : {
+  tooltip: {
     theme: isDark.value ? 'dark' : 'light',
   }
 });
@@ -236,11 +183,11 @@ const statisticsInfo = ref([
 watch(
   isDark,
   (newValue) => {
-    if(chartOptions.value.tooltip){
-      chartOptions.value.tooltip.theme = newValue ? 'dark' : 'light'
+    if (chartOptionsSuccess.value.tooltip) {
+      chartOptionsSuccess.value.tooltip.theme = newValue ? 'dark' : 'light'
     }
-    if(chartOptions3.value.tooltip){
-      chartOptions3.value.tooltip.theme = newValue ? 'dark' : 'light'
+    if (chartOptionsError.value.tooltip) {
+      chartOptionsError.value.tooltip.theme = newValue ? 'dark' : 'light'
     }
   }
 )
@@ -248,49 +195,6 @@ watch(
 
 <style scoped>
 
-
-.chart-wrapper {
-  margin: -40px -20px;
-}
-
-.avatar-wrapper {
-  width: 48px;
-  height: 48px;
-  border-radius: 50%;
-  background-color: color-mix(in srgb, var(--primary-color), var(--bg-primary) 75%);
-  color: var(--primary-color);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.card-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-}
-
-.card-header h3 {
-  font-size: 16px;
-  font-weight: 500;
-  color: var(--text-primary) !important;
-}
-
-.card-header-actions {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 56px;
-  height: 56px;
-  background-color: color-mix(in srgb, var(--primary-color), var(--bg-primary) 75%);
-  border-radius: 18px;
-}
-
-.card-header-actions .n-icon {
-  color: var(--primary-color);
-  font-size: 24px;
-}
 
 .analytical-page {
   max-width: 100%;
@@ -310,39 +214,7 @@ watch(
 }
 
 :deep(.n-card) {
-  height: 100%;
-  background: var(--bg-primary);
+  height: 100% !important;
   border: 1px solid var(--border-color);
-}
-
-:deep(.n-card .n-card__content) {
-  padding: 16px;
-}
-
-.custom-badge {
-  display: inline-flex;
-  align-items: center;
-  padding: 2px 8px;
-  color: white;
-  border-radius: 12px;
-  font-size: 12px;
-  font-weight: 500;
-  margin-left: 8px;
-}
-
-
-[data-theme="dark"] .card-header-actions:hover {
-  box-shadow: 0 4px 12px rgba(99, 102, 241, 0.15);
-}
-
-[data-theme="dark"] .avatar-wrapper:hover {
-  box-shadow: 0 4px 12px rgba(99, 102, 241, 0.15);
-}
-
-
-@media (max-width: 768px) {
-  :deep(.n-grid) {
-    grid-template-columns: 1fr !important;
-  }
 }
 </style>
---
+++ src/views/dashboard/analytical/widgets/statisticsByChart.vue
@@ -0,0 +1,74 @@
+<template>
+    <n-card>
+        <div class="card-header">
+            <h3>{{ label }}</h3>
+            <div>
+                <n-icon :component="type === 'success' ? TrendingUpOutline : TrendingDownOutline" size="18"
+                    :color="type === 'success' ? 'var(--color-success)' : 'var(--color-error)'"></n-icon>
+                <span :class="`custom-badge bg-${type}`">+12.5%</span>
+            </div>
+        </div>
+        <h1>{{ value }}</h1>
+        <div class="chart-wrapper">
+            <apexchart type="area" height="150" :options="chartOptions" :series="series">
+            </apexchart>
+        </div>
+    </n-card>
+</template>
+<script lang="ts" setup>
+import { NCard, NIcon } from 'naive-ui';
+import { TrendingDownOutline, TrendingUpOutline } from '@vicons/ionicons5';
+import type { ApexOptions } from 'apexcharts';
+import VueApexCharts from 'vue3-apexcharts';
+
+const apexchart = VueApexCharts;
+
+interface IProps {
+    series: any[];
+    chartOptions: ApexOptions;
+    type: 'success' | 'error';
+    label: string;
+    value: string;
+}
+
+withDefaults(defineProps<IProps>(), {
+    type: 'success',
+    label: 'Users',
+    value: '28.05k'
+});
+</script>
+
+<style scoped>
+.chart-wrapper {
+    margin: -40px -20px;
+}
+
+.custom-badge {
+    display: inline-flex;
+    align-items: center;
+    padding: 2px 8px;
+    color: white;
+    border-radius: 12px;
+    font-size: 12px;
+    font-weight: 500;
+    margin-left: 8px;
+}
+
+
+
+:deep(.n-card .n-card__content) {
+  padding: 16px;
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+@media (max-width: 768px) {
+  :deep(.n-grid) {
+    grid-template-columns: 1fr !important;
+  }
+}
+</style>
